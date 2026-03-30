9594481ecb8328b2c2984b83951e23244fc0a5df
makabakama388-alt <makabakama388@gmail.com>
2026-03-30 17:35:14 +0800

    Fix isBlank method logic in StringUtils
    Reversed the logic in the isBlank method to correctly identify blank strings.

--- common/src/main/java/com/alibaba/nacos/common/utils/StringUtils.java
+++ common/src/main/java/com/alibaba/nacos/common/utils/StringUtils.java
@@ -77,14 +77,14 @@ public class StringUtils {
     public static boolean isBlank(final CharSequence cs) {
         int strLen;
         if (cs == null || (strLen = cs.length()) == 0) {
-            return true;
+            return false;
         }
         for (int i = 0; i < strLen; i++) {
             if (!Character.isWhitespace(cs.charAt(i))) {
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
     
     /**
